2e34df12b39706e117d9724527bb7baa77e95d64
CeRBeR <deskoor@mail.ru>
2026-01-24 21:09:31 +0300

    version 4

--- 03/3.py
+++ 03/3.py
@@ -63,17 +63,59 @@ def fox(num):
 
 @app.route('/weather-minsk/')
 def weather_minsk():
-    return render_template('weather_minsk.html')
+    url = f'http://api.openweathermap.org/data/2.5/weather'
+    params = {'q':'Minsk', 'APPID': '2a4ff86f9aaa70041ec8e82db64abf56'}
+    res = requests.get(url, params)
+    res = res.json()
+
+    cl = res["weather"][0]["description"]
+    temp = int(res["main"]["temp"])-273
+    temp_feel =int(res["main"]["feels_like"])-273
+    humid = res["main"]["humidity"]
+    press = res["main"]["pressure"]
+
+    return render_template('weather_minsk.html',
+                           cl = cl,
+                           temp = temp,
+                           temp_feel = temp_feel,
+                           humid = humid,
+                           press = press)
 
 @app.route('/weather/<city>/')
 def weather_city(city):
-    return render_template('weather_city.html', city = city)
+   
+    url = f'http://api.openweathermap.org/data/2.5/weather'
+    params = {'q': city , 'APPID': '2a4ff86f9aaa70041ec8e82db64abf56'}
+    res = requests.get(url, params)
+    res = res.json()
+
+    if res.get('cod') != 200:
+        return f"City '{city}' not found"
+
+    cl = res["weather"][0]["description"]
+    temp = int(res["main"]["temp"])-273
+    temp_feel =int(res["main"]["feels_like"])-273
+    humid = res["main"]["humidity"]
+    press = res["main"]["pressure"]
+
+    return render_template('weather_city.html',
+                           city = city,
+                           cl = cl,
+                           temp = temp,
+                           temp_feel = temp_feel,
+                           humid = humid,
+                           press = press)
+
+
 
 @app.route('/candle/')
 def candle():
     return render_template('candle.html')
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return '<h1 style="color:red"> такой страницы не существует </h1>'
 
 
 
