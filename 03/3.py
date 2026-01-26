a191aac7d93f04281e233c85b934811569dc2735
CeRBeR <deskoor@mail.ru>
2026-01-26 22:05:25 +0300

    version 4

--- 03/3.py
+++ 03/3.py
@@ -25,11 +25,12 @@
 
 
 from flask import (Flask, render_template,
-                   session, redirect, url_for)
+                   session, request, redirect, url_for)
 
 import requests
 import os
 import random
+import re
 
 BASE_DIR = os.path.dirname(__file__)
 
@@ -38,6 +39,38 @@ app = Flask(__name__,
             static_folder = os.path.join(BASE_DIR, 'static'))
 
 app.config['SECRET_KEY'] = 'my secret key'
+user_db = {}
+
+def login_check():
+    return 'user' in session
+
+def valid_regist(form):
+    err = []
+
+    name = form.get('name', '').strip()
+    surname = form.get('surname', '').strip()
+    login = form.get('login', '').strip()
+    password = form.get('password', '').strip()
+
+    if not re.fullmatch(r'[А-Яа-яЁё]+', name):
+        err.append('The name must contain only Russian letters.')
+    
+    if not re.fullmatch(r'[А-Яа-яЁё]+', surname):
+        err.append('The surname must contain only Russian letters.')
+    
+    if not re.fullmatch(r'[A-Za-z0-9_]{6,20}', login):
+        err.append('The login must contain Latin letters, numbers and _.' \
+                  ' The number of characters must be from 6 to 20 characters.')
+    
+    if login in user_db:
+        err.append('The login is already taken')
+    
+    if not re.fullmatch(r'(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,15}', password):
+        err.append('The password must contain at least one lowercase Latin letter, one uppercase letter, and one number. ' \
+                   'It must be between 8 and 15 characters long.')
+    
+    return err 
+
 
 @app.route('/')
 def index():
@@ -46,6 +79,9 @@ def index():
 
 @app.route('/duck/')
 def duck():
+    if not login_check():
+        return redirect(url_for('sign_up'))
+    
     res = requests.get("https://random-d.uk/api/random")
     img_url = res.json()['url']
     num_img = img_url.split('/')[-1].split('.')[0]
@@ -54,6 +90,9 @@ def duck():
 
 @app.route('/fox/<int:num>/')
 def fox(num):
+    if not login_check():
+        return redirect(url_for('sign_up'))
+
     if num < 1 or num > 10:
         return f"Only from 1 to 10 is allowed"
 
@@ -67,6 +106,9 @@ def fox(num):
 
 @app.route('/weather-minsk/')
 def weather_minsk():
+    if not login_check():
+        return redirect(url_for('sign_up'))
+
     url = f'http://api.openweathermap.org/data/2.5/weather'
     params = {'q':'Minsk', 'APPID': '2a4ff86f9aaa70041ec8e82db64abf56'}
     res = requests.get(url, params)
@@ -87,6 +129,8 @@ def weather_minsk():
 
 @app.route('/weather/<city>/')
 def weather_city(city):
+    if not login_check():
+        return redirect(url_for('sign_up'))
    
     url = f'http://api.openweathermap.org/data/2.5/weather'
     params = {'q': city , 'APPID': '2a4ff86f9aaa70041ec8e82db64abf56'}
@@ -114,19 +158,69 @@ def weather_city(city):
 
 @app.route('/candle/')
 def candle():
+    if not login_check():
+        return redirect(url_for('sign_up'))
+    
     return render_template('candle.html')
 
 @app.route('/regist/', methods = ['GET', 'POST'])
 def regist():
-    return render_template('register.html')
+    if login_check():
+        return redirect(url_for('index'))
+
+    err = []
+    form_data = {}
+
+    if request.method == 'POST':
+        form_data = request.form.to_dict()
+        err = valid_regist(request.form)
+
+        if not err:
+            user_db[form_data['login']] ={
+                'name': form_data['name'],
+                'surname': form_data['surname'],
+                'password': form_data['password']
+            }
+            return redirect(url_for('sign_up'))
+
+    return render_template('register.html', err = err , form = form_data)
 
 @app.route('/sign-up/', methods = ['GET', 'POST'])
 def sign_up():
-    return render_template('sign_up.html')
+    if login_check():
+        return redirect(url_for('index'))
+    
+    err = None
+
+    if request.method == 'POST':
+        login_val = request.form.get('login')
+        password = request.form.get('password')
+
+        user = user_db.get(login_val)
+
+        if user and user['password'] == password:
+            session['user'] = {
+                'login': login_val,
+                'name': user['name'],
+                'surname': user['surname']
+            }
+            return redirect(url_for('index'))
+        
+        else:
+            err = 'Incorrect login or password'
+            
+
+    return render_template('sign_up.html', err = err)
+
+
+@app.route('/logout/')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
 
 
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found(err):
     return '<h1 style="color:red"> такой страницы не существует </h1>'
 
 
