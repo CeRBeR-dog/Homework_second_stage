1f89c9012268976bbe611e14b75544be31bc818f
CeRBeR <deskoor@mail.ru>
2026-02-28 16:16:43 +0300

    version 14

--- 12/electronic_diary/main/urls.py
+++ 12/electronic_diary/main/urls.py
@@ -5,10 +5,14 @@ from . import views
 
 urlpatterns = [              
     path('', views.index, name='index'),
+
     path('students/', views.students, name='students'),
-    path('students/<int:student_id>/', views.student_detail, name='student_detail'), 
+    path('students/<int:student_id>/', views.student_detail, name='student_detail'),
+
     path('courses/', views.courses, name='courses'),  
-    path('courses/<int:course_id>/', views.course_detail, name='course_detail'), 
+    path('courses/<int:course_id>/', views.course_detail, name='course_detail'),
+    path('courses/<int:course_id>/journal/', views.course_journal, name='course_journal'), 
+    
     path('grades/', views.grades_journal, name='grades_journal'),  
     
     
--- 12/electronic_diary/main/views.py
+++ 12/electronic_diary/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import *
+from .models import Student, Course, Grade, Profile
+from collections import defaultdict
 
 # Create your views here.
 
@@ -14,7 +15,9 @@ def students(request):
 
 def student_detail(request, student_id):
     student = get_object_or_404(Student, id = student_id)
-    return render(request, 'main/student_detail.html', {'student': student})
+    return render(request, 'main/student_detail.html', 
+                  {'student': student}
+                  )
 
 def courses(request):
     course = Course.objects.all()
@@ -24,8 +27,36 @@ def courses(request):
 
 def course_detail(request, course_id):
     course = get_object_or_404(Course, id = course_id)
-    return render(request, 'main/course_detail.html', {'course': course})
+    return render(request, 
+                  'main/course_detail.html', 
+                  {'course': course}
+                  )
 
 def grades_journal(request):
     students = Student.objects.prefetch_related('grades')
-    return render(request, 'main/grades_journal.html', {'students': students})
+    return render(request,
+                    'main/grades_journal.html',
+                    {'students': students}
+                    )
+
+def course_journal(request, course_id):
+    course = get_object_or_404(Course, id = course_id)
+
+    students = course.students.all()
+    grades = Grade.objects.filter(course=course).select_related('person')
+
+    dates = sorted(set(grade.date for grade in grades))
+
+    journal = defaultdict(dict)
+
+    for grade in grades:
+        journal[grade.person.id][grade.date] = grade.grade
+
+    context = {
+        'course': course,
+        'students': students,
+        'dates': dates,
+        'journal': journal,
+    }
+
+    return render(request, 'main/course_journal.html', context)
