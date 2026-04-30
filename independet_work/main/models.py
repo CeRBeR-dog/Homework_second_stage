0cf08328b808e8ae95bf71b9ed32c19133efe9ee
CeRBeR <deskoor@mail.ru>
2026-04-30 21:51:23 +0300

    post version 17

--- independet_work/main/models.py
+++ independet_work/main/models.py
@@ -4,15 +4,71 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 
 # Create your models here.
 
+class Category(models.Model):
+
+    name_category = models.CharField(max_length=100,
+                            verbose_name='Категория товара',
+                            null=False,
+                            blank=False,
+                            )
+    
+    slug = models.SlugField(max_length=100,
+                            unique=True,
+                            verbose_name='URL',
+                            )
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Scent(models.Model):
+
+    name_scent = models.CharField(max_length=100,
+                            verbose_name='Название аромата',
+                            null=False,
+                            blank=False,
+                            )
+    
+    notes = models.TextField(verbose_name='Описание нот',
+                                    blank=True,
+                                    )
+    
+    class Meta:
+        verbose_name = 'Аромат'
+        verbose_name_plural = 'Ароматы'
+
+
+# class Productimage(models.Model):
+
+
+
 class Craftitem(models.Model):
 
-    name = models.CharField(max_length=50,
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE,
+                                 related_name='name_category',
+                                 verbose_name='Категория',
+                                 )
+    
+    scent = models.ForeignKey(Scent,
+                              on_delete=models.CASCADE,
+                              related_name='name_scent',
+                              verbose_name='Аромат',
+                              )
+
+    name_item = models.CharField(max_length=100,
                             verbose_name='Имя товара',
                             null=False,
                             blank=False,
                             )
     
-    photo = models.ImageField(upload_to='photo_item',
+    slug = models.SlugField(max_length=100,
+                            unique=True,
+                            verbose_name='URL',
+                            )
+    
+    photo = models.ImageField(upload_to='products/%Y/%m/%d/',
                               blank=True,
                               null=True,
                               verbose_name='Фото товара',
@@ -24,24 +80,62 @@ class Craftitem(models.Model):
                        verbose_name='Цена товара',
                        )
     
-    availability = models.BooleanField(verbose_name='Наличие товара',
-                                       null=True,
-                                       blank=True,
+    availability = models.BooleanField(verbose_name='В наличии',
+                                       default=True,
                                        )
     
     description = models.TextField(verbose_name='Описание товара',
                                     blank=True,
-                                    null=True,
                                     )
     
     class Meta:
-        verbos_name = 'Товар',
-        verbos_name_plural = 'Товары'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.name
+    
+
+class Lead(models.Model):
+
+    choices_status = [
+        ('new','новый'),
+        ('in_progress','делается'),
+        ('done','готово')
+               ]
+        
+    product = models.ForeignKey(Craftitem,
+                                on_delete=models.CASCADE,
+                                related_name='name_item',
+                                verbose_name='Название товара для заказа',
+                                )
+    
+    name_client = models.CharField(max_length=50,
+                            verbose_name='Имя заказчика',
+                            null=False,
+                            blank=False,
+                            )
+    
+    contact_info = models.CharField(max_length=100,
+                            verbose_name='Контакты заказчика',
+                            null=False,
+                            blank=False,
+                            )
+    
+    status = models.CharField(choices=choices_status,
+                              max_length=20,
+                              verbose_name='Статус заказа',
+                              )
+    
+    created_at = models.DateTimeField(verbose_name='Дата и время подачи заявки',
+                                      null=True,
+                                      )
 
 
-    # #заглушка на отсутсвующие фото
-    # @property
-    # def get_photo_url(self):
-    #     if self.photo and self.photo.storage.exists(self.photo.name):
-    #         return self.photo.url
-    #     return staticfiles_storage.url('images/no_photo.jpg')
+    #заглушка на отсутсвующие фото
+    @property
+    def get_photo_url(self):
+        if self.photo and hasattr(self.photo, 'url'):
+            return self.photo.url
+        return staticfiles_storage.url('images/no_photo.jpg')
